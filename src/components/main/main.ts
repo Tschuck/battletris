c1f5f1e9114534a532205a747f940ef1a0930eeb
Tobias Winkler <tobias.winkler@evan.team>
2019-09-01 11:40:25 +0200

    remove empty classes and fill no available classes with default one

--- src/components/main/main.ts
+++ src/components/main/main.ts
@@ -18,6 +18,12 @@ export default class Main extends Vue {
       await battletris.initialize();
       this.$store.state.userConfig = await battletris.getUserConfig();
       this.$store.state.classes = await battletris.getClasses();
+
+      // check if user class is available, else use the first one
+      const classNames = Object.keys(this.$store.state.classes);
+      if (classNames.indexOf(this.$store.state.userConfig.className) === -1) {
+        this.$store.state.userConfig.className = classNames[0];
+      }
     } catch (ex) {
       this.error = ex.message;
     }
