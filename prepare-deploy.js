8abde653994f13c29ea310dd4541db6f418ff28f
Tobias Winkler <tobias.winkler@evan.team>
2020-12-03 09:10:12 +0100

    heroku deploy

---
+++ prepare-deploy.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+
+const deployFolder = path.resolve('./.deploy');
+const backendFolder = path.resolve('./backend');
+const frontendFolder = path.resolve('./frontend');
+const package = {
+  ...require(`${backendFolder}/package.json`),
+  devDependencies: {},
+  scripts: {
+    start: "node dist/src/index.js"
+  },
+};
+
+fse.emptyDirSync(deployFolder);
+fse.ensureDirSync(deployFolder);
+
+// setup base files
+fs.writeFileSync(`${deployFolder}/package.json`, JSON.stringify(package, null, 2), 'utf8');
+
+// copy backend files first
+[
+  'dist',
+  'yarn.lock',
+].forEach((dirName) => fse.copySync(`${backendFolder}/${dirName}`, `${deployFolder}/${dirName}`));
+// copy built frontend files
+fse.copySync(`${frontendFolder}/dist`, `${deployFolder}/dist/public`);
